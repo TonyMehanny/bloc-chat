3736151795023109023b1e14fd637738a9d18abb
Tony Mehanny <amehanny@equinix.com>
2017-08-25 11:47:30 -0400

    Checkpoint 2 Completed

---
+++ app/scripts/controllers/HomeCtrl.js
@@ -0,0 +1,10 @@
+(function() {
+    function HomeCtrl(Room) {
+      this.Rooms = Room.all;
+      console.log(this.Rooms);
+    }
+
+    angular
+        .module('blocChat')
+        .controller('HomeCtrl', ['Room',HomeCtrl]);
+})();
